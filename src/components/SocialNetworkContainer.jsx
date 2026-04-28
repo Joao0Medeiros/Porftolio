a1a91f072ab4705c3a7faec59f2bbfcc1ba25e8c
Joao Felipe Melo Medeiros <131771474+Joao0Medeiros@users.noreply.github.com>
2026-04-28 00:19:07 -0300

    Update SocialNetworkContainer.jsx

--- src/components/SocialNetworkContainer.jsx
+++ src/components/SocialNetworkContainer.jsx
@@ -3,9 +3,9 @@ import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
 import "../styles/components/socialnetworkcontainer.sass";
 
 const socialNetworks = [
-  { name: "linkedin", icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/joão-felipe-297934256/" },
+  { name: "linkedin", icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/joão-medeiros-297934256/" },
   { name: "github", icon: <FaGithub />, link: "https://github.com/Joao0Medeiros" },
-  { name: "instagram", icon: <FaInstagram />, link: "https://www.instagram.com/juao.med/" },
+  { name: "instagram", icon: <FaInstagram />, link: "https://www.instagram.com/joaofm_//" },
 ];
 
 const SocialNetworkContainer = () => {
